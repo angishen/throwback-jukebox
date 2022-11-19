e5643a3cf13bac070a830d092ec47ed5557e16c5
Angela Shen <angelashen01@gmail.com>
2022-11-19 00:19:55 -0800

    Add sql table and data loading queries and update python script.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # throwback-jukebox
+
+### Start MySQL Server Locally
+> sudo mysql.server start
+> mysql --local-infile=1 -u root
+
+To enable local infiles, in mysql, run:
+> set global local_infile=true
--- process_data.py
+++ process_data.py
@@ -1,25 +1,43 @@
 import pandas as pd
+import re
 from datetime import date, timedelta
 
 def generate_week_start_date(end_date_str):
     end_date = date.fromisoformat(end_date_str)
-    return end_date - timedelta(days=7)
+    start_date = end_date - timedelta(days=6)
+    return start_date.strftime("%Y-%m-%d")
 
-hot_100 = pd.read_csv("./hot_stuff_2.csv")
+def remove_secondary_artists(artist_str):
+    artists = re.split(r'(\s|\s\(|\s\[)featuring\s', artist_str, flags=re.I)
 
-weekid = hot_100["weekid"]
-hot_100["week_start_date"] = weekid.apply(generate_week_start_date)
+    return artists[0]
 
-hot_100 = hot_100[[
-    "weekid",
-    "week_start_date",
-    "song",
-    "performer",
-    "week_position",
-    "previous_week_position",
-    "peak_position",
-    "weeks_on_chart",
-    "instance"
-]]
+try:
+    data = pd.read_csv("./charts.csv")
 
-hot_100.to_csv("./hot_100_processed.csv", index=False)
+    week_end_date = data["date"]
+    data["week_start_date"] = week_end_date.apply(generate_week_start_date)
+
+    artist  = data["artist"]
+    data["primary_artist"] = artist.apply(remove_secondary_artists)
+
+    data = data.rename(columns={
+        "date": "week_end_date",
+        "rank": "song_rank",
+        "song": "song_name",
+        "artist": "artist_display_name"
+    })
+
+    data = data[[
+        "week_end_date",
+        "week_start_date",
+        "song_rank",
+        "song_name",
+        "primary_artist",
+        "artist_display_name"
+    ]]
+
+    data.to_csv("./charts_processed.csv", index=False)
+    print("Successfully parsed input file")
+except Exception as e:
+    print("Failed to process input file", e)
